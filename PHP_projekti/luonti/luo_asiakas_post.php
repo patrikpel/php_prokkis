a434509fa9f88850eb25227e7c07b1abae2a0513
patrikpel <40035823+patrikpel@users.noreply.github.com>
2022-01-05 22:28:36 +0200

    Add files via upload

---
+++ PHP_projekti/luonti/luo_asiakas_post.php
@@ -0,0 +1,20 @@
+<?php
+include_once '../connect.php';
+
+$name = $_POST['name'];
+$comp = $_POST['comp'];
+$job = $_POST['job'];
+$e_mail = $_POST['e_mail'];
+
+if($conn->connect_error){
+    die('Yhteys epäonnistui: '.$conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT into asiakkaat(nimi, yritys, toimenkuva, email)
+    VALUES(?,?,?,?)");
+    $stmt->bind_param("ssss",$name,$comp,$job,$e_mail);
+    $stmt->execute();
+    $stmt->close();
+    header("Location: ./luo_asiakas.php?create=true");
+}
+
+?>
